93776a84d915a5090483776e13ad9c003b6eadd7
daniel mishael <dmishael91@gmail.com>
2019-10-04 16:04:47 -0400

    cleaned up

--- src/js/App.js
+++ src/js/App.js
@@ -1,32 +1,28 @@
-/* eslint-disable indent */
-import React, { useState, useEffect, Component } from 'react'
-import "whatwg-fetch"
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.getPHP = this.getPHP.bind(this);
-  }
-
-  getPHP() {
-    fetch("http://localhost:8000/webservice.php", {
-      method: "GET",
-      headers: {},
-      body: JSON.stringify({
-        content: "test1"
-      })
-    })
-      .then(res => res.json())
-      .then(response => {
-        console.log("response:");
-        console.log(response);
-      })
-
-
-  render() {
-    return <div>hi</div>
-  }
-}
+function App() {
+  const [data, setData] = useState({ hits: [] })
+  useEffect(() => {
+    console.log('component mounted')
+    const fetchData = async () => {
+      const result = await axios('http://localhost:8000/webservice.php')
+      console.log('component mounted')
+      setData(result.data.nodes)
+    }
+    fetchData()
+  }, [])
+  return (
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <div>
+            <img src={item.thumbnail.href} alt={item.thumbnail.description} />
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default App
